ef01256aa16772b3a85efa0774b9e069ab6bd2d1
Owner <mahmoudelgmal991@gmail.com>
2023-10-03 01:48:34 +0300

    use the data from api in the ui

--- lib/services/get_news.dart
+++ lib/services/get_news.dart
@@ -4,18 +4,19 @@ import 'package:news_app_ui_setup/models/articale_model.dart';
 class GetNews {
   final dio = Dio();
 
-  void getHttp() async {
+  Future<List<ArticleModel>> getHttp() async {
     final response = await dio.get(
-        'https://newsapi.org/v2/top-headlines?country=eg&apiKey=301048b330f04c1caa56c44578b22af2&category=general');
+        'https://newsapi.org/v2/top-headlines?country=us&apiKey=301048b330f04c1caa56c44578b22af2');
     Map<String, dynamic> jsonData = response.data;
-    List<dynamic> article=jsonData["articles"];
-    List<ArticleModel> articles=[];
-    for(var art in article){
-    ArticleModel obj=  ArticleModel(title:art["title"]  ,image:art["url"], supTitle:art["description"] );
+    List<dynamic> article = jsonData["articles"];
+    List<ArticleModel> articles = [];
+    for (var art in article) {
+      ArticleModel obj = ArticleModel(
+          title: art["title"], image: art["urlToImage"], supTitle: art["description"]);
       articles.add(obj);
     }
+    return articles;
 
-
-   // print(article);
+    // print(article);
   }
 }
--- lib/widgets/list_view_tile.dart
+++ lib/widgets/list_view_tile.dart
@@ -1,14 +1,36 @@
 import 'package:flutter/cupertino.dart';
+import 'package:news_app_ui_setup/services/get_news.dart';
 import 'package:news_app_ui_setup/widgets/news_container.dart';
 
-class ListTilee extends StatelessWidget {
+import '../models/articale_model.dart';
+
+class ListTilee extends StatefulWidget {
   const ListTilee({Key? key}) : super(key: key);
 
   @override
+  State<ListTilee> createState() => _ListTileeState();
+}
+
+class _ListTileeState extends State<ListTilee> {
+  List<ArticleModel> arts=[];
+  @override
+  void initState(){
+    super.initState();
+    geGeneralNews();
+  }
+
+  Future<void> geGeneralNews() async {
+     arts=  await GetNews().getHttp();
+     setState(() {
+
+     });
+
+  }
+  @override
   Widget build(BuildContext context) {
     return SliverList.separated(
-      itemBuilder: (context, index) => NewsTile(),
-      separatorBuilder: (context, index) => SizedBox(height: 10),
+      itemBuilder: (context, index) => NewsTile(arts[index]),
+      separatorBuilder: (context, index) => const SizedBox(height: 10),
       itemCount: 10,
     );
   }
